39fc715bfcecb28cd7f0c4fe6824f107cbd20f07
johndeereranger <370Smithfarms@gmail.com>
2025-01-17 10:55:04 -0500

    update 1.0.1

--- Package.swift
+++ Package.swift
@@ -5,11 +5,18 @@ import PackageDescription
 
 let package = Package(
     name: "UserInfoLibrary",
+    platforms: [
+           .iOS(.v13) // Ensure iOS 13 is the minimum deployment target
+       ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "UserInfoLibrary",
             targets: ["UserInfoLibrary"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
