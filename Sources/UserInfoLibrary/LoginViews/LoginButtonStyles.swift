e3b3457c20725978b40a0d2e79d30bc6e4ad977d
johndeereranger <370Smithfarms@gmail.com>
2025-01-27 11:33:55 -0500

    ButtonStyle Small Tweak

--- Sources/UserInfoLibrary/LoginViews/LoginButtonStyles.swift
+++ Sources/UserInfoLibrary/LoginViews/LoginButtonStyles.swift
@@ -42,47 +42,47 @@ public struct ActionButtonBlueButtonStyle: ButtonStyle {
     }
 }
 
-
-extension View {
-
-    
-    public func actionButtonBlueButtonStyle() -> some View {
-         self.modifier(ActionButtonBlueButtonStyle())
-     }
-    
-    public func resetPasswordLightGreyButtonStyle() -> some View {
-        self.modifier(PasswordResetLightGreyButtonStyle())
-    }
-}
-
-public struct PasswordResetLightGreyButtonStyle: ViewModifier {
-    public init() {}
-    public func body(content: Content) -> some View {
-        content
-            .frame(maxWidth: .infinity)
-            .foregroundColor(.black) // Text color
-            .font(.headline) // Uses the system's headline font
-            .padding(.vertical, 10)
-            
-            .padding(.horizontal, 20) // Adjust padding as needed
-            .background(Color(red: 229/255, green: 229/255, blue: 234/255)) // Custom green background
-            .cornerRadius(10) // Rounded corners
-            //.scaleEffect(configuration.isPressed ? 0.95 : 1.0) // Optional: Slight scale down when pressed
-    }
-}
-
-public struct ActionButtonBlueButtonStyle: ViewModifier {
-    public init() {}
-    public func body(content: Content) -> some View {
-        content
-            .frame(maxWidth: .infinity)
-            .padding(.vertical, 10)
-            .padding(.horizontal, 20) // Adjust padding as needed
-            .contentShape(Rectangle())
-            .background(Color(red: 39/255, green: 68/255, blue: 114/255)) // Custom green background
-            .foregroundColor(.white) // Text color
-            .font(.headline)
-            .cornerRadius(10) // Rounded corners
-            //.scaleEffect(configuration.isPressed ? 0.95 : 1.0) // Optional: Slight scale down when pressed
-    }
-}
+//
+//extension View {
+//
+//    
+//    public func actionButtonBlueButtonStyle() -> some View {
+//         self.modifier(ActionButtonBlueButtonStyle())
+//     }
+//    
+//    public func resetPasswordLightGreyButtonStyle() -> some View {
+//        self.modifier(PasswordResetLightGreyButtonStyle())
+//    }
+//}
+//
+//public struct PasswordResetLightGreyButtonStyle: ViewModifier {
+//    public init() {}
+//    public func body(content: Content) -> some View {
+//        content
+//            .frame(maxWidth: .infinity)
+//            .foregroundColor(.black) // Text color
+//            .font(.headline) // Uses the system's headline font
+//            .padding(.vertical, 10)
+//            
+//            .padding(.horizontal, 20) // Adjust padding as needed
+//            .background(Color(red: 229/255, green: 229/255, blue: 234/255)) // Custom green background
+//            .cornerRadius(10) // Rounded corners
+//            //.scaleEffect(configuration.isPressed ? 0.95 : 1.0) // Optional: Slight scale down when pressed
+//    }
+//}
+//
+//public struct ActionButtonBlueButtonStyle: ViewModifier {
+//    public init() {}
+//    public func body(content: Content) -> some View {
+//        content
+//            .frame(maxWidth: .infinity)
+//            .padding(.vertical, 10)
+//            .padding(.horizontal, 20) // Adjust padding as needed
+//            .contentShape(Rectangle())
+//            .background(Color(red: 39/255, green: 68/255, blue: 114/255)) // Custom green background
+//            .foregroundColor(.white) // Text color
+//            .font(.headline)
+//            .cornerRadius(10) // Rounded corners
+//            //.scaleEffect(configuration.isPressed ? 0.95 : 1.0) // Optional: Slight scale down when pressed
+//    }
+//}
