6a68afa84fd23659f960eae9439df9edbe3bc445
johndeereranger <370Smithfarms@gmail.com>
2025-02-06 09:07:04 -0500

    added notification permission Manager

---
+++ Sources/UserInfoLibrary/Permissions/File.swift
@@ -0,0 +1,34 @@
+//
+//  NotificationPermissionManager.swift
+//  UserInfoLibrary
+//
+//  Created by Byron Smith on 2/6/25.
+//
+
+import UserNotifications
+import UIKit
+
+public class NotificationPermissionManager {
+   
+
+    private init() {}
+
+    /// Requests push notification permissions from the user.
+    public func requestPushNotifications() {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+            print("Permission granted: \(granted)")
+            guard granted else { return }
+            
+            UNUserNotificationCenter.current().getNotificationSettings { settings in
+                print("Notification settings: \(settings)")
+                guard settings.authorizationStatus == .authorized else { return }
+                
+                DispatchQueue.main.async {
+                    UIApplication.shared.registerForRemoteNotifications()
+                    UserUpdateManager.shared.setIsPushNoticationEnabled(isEnabled: true)
+                }
+            }
+        }
+    }
+}
+
